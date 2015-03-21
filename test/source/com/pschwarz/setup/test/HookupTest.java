6daa33657b0b4decc8df4f8235fa4c7d50e9eac6
Philip Schwarz <philip.johann.schwarz@gmail.com>
2015-03-21 15:20:33 +0000

    IDEA test runner does what we want

---
+++ test/source/com/pschwarz/setup/test/HookupTest.java
@@ -0,0 +1,15 @@
+package com.pschwarz.setup.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by pschwarz on 21/03/15.
+ */
+public class HookupTest
+{
+    @Test
+    public void isThisThingOn() throws Exception
+    {
+    }
+}
