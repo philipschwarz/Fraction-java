0369c3d01dfb3f6c7231a0c72df7ddf5d4c241f0
pschwarz <philip.johann.schwarz@gmail.com>
2015-04-04 11:53:58 +0100

    we no longer need the integerValue field

--- production/source/com/pschwarz/math/Fraction.java
+++ production/source/com/pschwarz/math/Fraction.java
@@ -2,32 +2,29 @@ package com.pschwarz.math;
 
 public class Fraction
 {
-    private final int denominator;
-    private final int integerValue;
     private final int numerator;
+    private final int denominator;
 
     public Fraction(int integerValue)
     {
-        this.integerValue = integerValue;
         this.numerator = integerValue;
         this.denominator = 1;
     }
 
     public Fraction(int numerator, int denominator)
     {
-        this.integerValue = numerator;
         this.numerator = numerator;
         this.denominator = denominator;
     }
 
     public Fraction plus(Fraction that)
     {
-        return new Fraction(this.integerValue + that.integerValue, denominator);
+        return new Fraction(this.numerator + that.numerator, denominator);
     }
 
     public int intValue()
     {
-        return integerValue;
+        return numerator;
     }
 
     public int getNumerator() { return numerator; }
