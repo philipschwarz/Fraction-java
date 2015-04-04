1bf850c436a0fcc77d2c558290657ffc1d9d9cad
pschwarz <philip.johann.schwarz@gmail.com>
2015-04-04 13:59:38 +0100

    replaced remaining scaffolding assertions with assertEquals()

--- test/source/com/pschwarz/math/AddFractionsTest.java
+++ test/source/com/pschwarz/math/AddFractionsTest.java
@@ -10,19 +10,19 @@ public class AddFractionsTest
     @Test
     public void zeroPlusZero() throws Exception
     {
-        assertEquals(0, new Fraction(0).plus(new Fraction(0)).intValue());
+        assertEquals(new Fraction(0), new Fraction(0).plus(new Fraction(0)));
     }
 
     @Test
     public void nonZeroPlusZero() throws Exception
     {
-        assertEquals(3, new Fraction(3).plus(new Fraction(0)).intValue());
+        assertEquals(new Fraction(3), new Fraction(3).plus(new Fraction(0)));
     }
 
     @Test
     public void zeroPlusNonZero() throws Exception
     {
-        assertEquals(5, new Fraction(0).plus(new Fraction(5)).intValue());
+        assertEquals(new Fraction(5), new Fraction(0).plus(new Fraction(5)));
     }
 
     @Test
@@ -42,4 +42,5 @@ public class AddFractionsTest
     {
         assertEquals(new Fraction(3, 5), new Fraction(1, 5).plus(new Fraction(2, 5)));
     }
+
 }
