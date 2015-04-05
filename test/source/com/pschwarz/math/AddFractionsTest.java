a2f61b95631fd4b45da9919dddcbec76eeffc31a
pschwarz <philip.johann.schwarz@gmail.com>
2015-04-05 22:48:25 +0100

    wrote a handful more examples for reducing while adding fractions, found a missing case for GCD.

--- test/source/com/pschwarz/math/AddFractionsTest.java
+++ test/source/com/pschwarz/math/AddFractionsTest.java
@@ -1,5 +1,6 @@
 package com.pschwarz.math;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -49,4 +50,41 @@ public class AddFractionsTest
         assertEquals(new Fraction(5, 6), new Fraction(1, 2).plus(new Fraction(1, 3)));
     }
 
+    @Test
+    public void reduceResultToWholeNumber() throws Exception
+    {
+        assertEquals(new Fraction(1), new Fraction(1, 3).plus(new Fraction(2, 3)));
+    }
+
+    @Test
+    public void oneDenominatorIsMultipleOfTheOther() throws Exception
+    {
+        assertEquals(new Fraction(11, 8), new Fraction(3, 4).plus(new Fraction(5, 8)));
+    }
+
+    @Test
+    public void commonFactorsInDenominators() throws Exception
+    {
+        assertEquals(new Fraction(11, 18), new Fraction(1, 6).plus(new Fraction(4, 9)));
+    }
+
+    @Test
+    public void reduceResultsEvenWhenDenominatorsAreTheSame() throws Exception
+    {
+        assertEquals(new Fraction(3, 2), new Fraction(3, 4).plus(new Fraction(3, 4)));
+    }
+
+    @Test
+    public void negativeFractionAndReducing() throws Exception
+    {
+        assertEquals(new Fraction(1, 2), new Fraction(-1, 4).plus(new Fraction(3, 4)));
+        assertEquals(new Fraction(-1, 8), new Fraction(3, 8).plus(new Fraction(-1, 2)));
+    }
+
+    @Test
+    @Ignore("Constructor doesn't yet avoid negative denominators")
+    public void negativeSignsEverywhere() throws Exception
+    {
+        assertEquals(new Fraction(1, 2), new Fraction(1, -4).plus(new Fraction(-3, -4)));
+    }
 }
