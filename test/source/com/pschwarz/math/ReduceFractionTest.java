9c01ece4d3c51b9d702e1f8ceb7c092d28953b3b
pschwarz <philip.johann.schwarz@gmail.com>
2015-04-05 22:13:25 +0100

    we now reduce fraction to whole number, if possible

---
+++ test/source/com/pschwarz/math/ReduceFractionTest.java
@@ -0,0 +1,28 @@
+package com.pschwarz.math;
+
+import com.pschwarz.math.Fraction;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ReduceFractionTest
+{
+    @Test
+    public void alreadyInLowestTerms() throws Exception
+    {
+        assertEquals(new Fraction(3, 4), new Fraction(3, 4));
+    }
+
+    @Test
+    public void reduceToNotWholeNumber() throws Exception
+    {
+        assertEquals(new Fraction(3, 4), new Fraction(6, 8));
+    }
+
+    @Test
+    public void reduceWholeNumber() throws Exception
+    {
+        assertEquals(new Fraction(6), new Fraction(24, 4));
+    }
+}
