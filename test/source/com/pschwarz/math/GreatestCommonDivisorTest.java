881a8edc22f2a503f3f6d1972a8d546c530fe3db
pschwarz <philip.johann.schwarz@gmail.com>
2015-04-05 21:41:10 +0100

    explored gcd algorithm from wikipedia - found an annoying aspect of its contract

--- test/source/com/pschwarz/math/GreatestCommonDivisorTest.java
+++ test/source/com/pschwarz/math/GreatestCommonDivisorTest.java
@@ -7,9 +7,43 @@ import static org.junit.Assert.assertEquals;
 public class GreatestCommonDivisorTest
 {
     @Test
-    public void oneAndOne() throws Exception
+    public void reflexive() throws Exception
     {
         assertEquals(1, gcd(1, 1));
+        assertEquals(2, gcd(2, 2));
+        assertEquals(-1, gcd(-1, -1));
+    }
+
+    @Test
+    public void relativelyPrime() throws Exception
+    {
+        assertEquals(1, gcd(2, 3));
+        assertEquals(1, gcd(4, 7));
+        assertEquals(-1, gcd(-2, -3));
+    }
+
+    @Test
+    public void oneIsMultipleOfTheOther() throws Exception
+    {
+        assertEquals(3, gcd(3, 9));
+        assertEquals(5, gcd(5, 30));
+    }
+
+    @Test
+    public void commonFactor() throws Exception
+    {
+        assertEquals(2, gcd(6, 8));
+        assertEquals(7, gcd(49, 315));
+        assertEquals(-4, gcd(-24, -28));
+    }
+
+    @Test
+    public void negatives() throws Exception
+    {
+        // Not sure if I like this or not
+        // probably prefer gcd to always be positive
+        assertEquals(4, gcd(-24, 28));
+        assertEquals(-4, gcd(24, -28));
     }
 
     private int gcd(int a, int b)
